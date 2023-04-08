63c269e13dc326ebcb6a66b7ed910d4997daa8c6
frdrk00 <masterkooo@gmail.com>
2023-04-08 21:32:59 +0200

    Get and show recipes

---
+++ src/server/api/routers/recipe.ts
@@ -0,0 +1,16 @@
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const recipeRouter = createTRPCRouter({
+    all: protectedProcedure.query( async ({ ctx }) => {
+        const recipes = await ctx.prisma.recipe.findMany({
+            where: {
+                userId: ctx.session.user.id
+            },
+            include: {
+                ingredients: true
+            }
+        })
+
+        return recipes.map(({ id, title, ingredients}) => ({ id, title, ingredients}))
+    })
+})
